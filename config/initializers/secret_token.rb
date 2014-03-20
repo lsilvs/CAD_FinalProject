b2866975c2b97c1d661335575de26e8ff4b64d85
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-03-20 23:16:12 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kamarad::Application.config.secret_token = '1da38ad664b33c0752ea05e12f91a849b8e6cb1c466963d4b0aee086a1388b9c3bdaac827223695d4afc233c574952b01d7bae9b70c88303df31f418f4df29bd'
