e82f0f36fed42ad2e4eac57c427a96a3a23ed084
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-03-29 00:43:20 +0000

    Apply a bootstrap theme

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -9,15 +9,13 @@ class ActionsController < ApplicationController
 		if params.has_key?(:commit)
 			current_user.credit = current_user.credit.to_f + params[:user][:credit].to_f
 			current_user.save
-			render "index"
-		else
-			render :layout => false	
 		end
+		render "index"
 	end
 
 	def transfer
 		if params.has_key?(:commit)
-			if to = User.find_by_phone_number(params[:user][:to_phone_number].to_s)
+			if to = User.find_by_phone_number(params[:user][:transfer_to].to_s)
 
 				to.credit = to.credit.to_f + params[:user][:amount].to_f
 
@@ -28,11 +26,8 @@ class ActionsController < ApplicationController
 			else
 				alert = "error"
 			end
-			
-			render "index"
-		else
-			render :layout => false	
 		end
+		render "index"
 	end
 
 end
